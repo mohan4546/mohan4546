94484cb7bb71c78d8c757ba6492987e124421c97
mohan4546 <mohan.dachepalli777@gmail.com>
2022-04-09 17:53:45 +0530

    Update and rename README.md to mohan.py

--- mohan.py
+++ mohan.py
@@ -8,3 +8,4 @@
 mohan4546/mohan4546 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
+print("hello world")
